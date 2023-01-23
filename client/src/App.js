b72712e8039bc6c6acd23e65151973529d4a9f7a
neyaznafiz <neyaznafiz@outlook.com>
2023-01-23 22:18:58 +0600

    file setup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from "react-toastify";
+import router from './Router/Routes/Routes';
+
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+
+  return (
+    <div className='max-w-screen-xl mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
